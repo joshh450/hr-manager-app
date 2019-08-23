79a468e8f892641a3d244b3f0eed4724bd1a6b6d
joshh450 <joshh450@yahoo.com>
2019-08-23 15:04:43 -0600

    prepared for deployment to heroku

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 app.use(express.json())
 
 connectDB();
 
-app.get('/', (req, res) => res.send('API running'))
-
 app.use('/api/employees', require('./routes/api/employees'))
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html');
+})
 
 const PORT = process.env.PORT || 5000;
 
