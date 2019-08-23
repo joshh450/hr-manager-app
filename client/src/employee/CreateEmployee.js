6b4e432532e0332feb5e854e58d18965d9925fd8
joshh450 <joshh450@yahoo.com>
2019-08-22 19:59:18 -0600

    changed the create and edit components to have input fields

--- client/src/employee/CreateEmployee.js
+++ client/src/employee/CreateEmployee.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faEnvelope, faPhone, faIdCard } from '@fortawesome/free-solid-svg-icons'
 
 const CreateEmployee = props => {
     return (
-        <div>
-            Create
+        <div className="employee-list">
+            <div className="spacer-25"></div>
+            <div className="employee-list__container">
+                <h2 className="employee-list__container__title">Create Employee</h2>
+                <div className="employee-card">
+                    <div className="employee-field"><FontAwesomeIcon icon={faUser} size="2x"/><input className="input-field" type="text" placeholder="Full Name"></input></div>
+                    <div className="employee-field"><FontAwesomeIcon icon={faEnvelope} size="2x"/><input className="input-field" type="email" placeholder="Email Address"></input></div>
+                    <div className="employee-field"><FontAwesomeIcon icon={faPhone} size="2x"/><input className="input-field" type="text" placeholder="Phone Number"></input></div>
+                    <div className="employee-field"><FontAwesomeIcon icon={faIdCard} size="2x"/><input className="input-field" type="text" placeholder="Employee ID"></input></div>
+                    <div className="employee-field__buttons"><button className="button__edit">Create</button></div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/employee/EditEmployee.js
+++ client/src/employee/EditEmployee.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faEnvelope, faPhone, faIdCard } from '@fortawesome/free-solid-svg-icons'
 
 const EditEmployee = props => {
     return (
-        <div>
-            Edit
+        <div className="employee-list">
+            <div className="spacer-25"></div>
+            <div className="employee-list__container">
+                <h2 className="employee-list__container__title">Edit Employee</h2>
+                <div className="employee-card">
+                <div className="employee-field"><FontAwesomeIcon icon={faUser} size="2x"/><input className="input-field" type="text" placeholder="Full Name"></input></div>
+                    <div className="employee-field"><FontAwesomeIcon icon={faEnvelope} size="2x"/><input className="input-field" type="email" placeholder="Email Address"></input></div>
+                    <div className="employee-field"><FontAwesomeIcon icon={faPhone} size="2x"/><input className="input-field" type="text" placeholder="Phone Number"></input></div>
+                    <div className="employee-field"><FontAwesomeIcon icon={faIdCard} size="2x"/><input className="input-field" type="text" placeholder="Employee ID"></input></div>
+                    <div className="employee-field__buttons"><button className="button__delete">Delete</button></div>
+                    <div className="employee-field__time">Began working 10 months ago</div>
+                </div>
+            </div>
         </div>
     )
 }
